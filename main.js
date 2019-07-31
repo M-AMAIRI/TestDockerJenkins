eaf3b9bce74c9d99aadaf45a04bdf08ec55c8f63
moatez amairi <35851660+M-AMAIRI@users.noreply.github.com>
2019-07-31 17:06:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -9,4 +9,4 @@ var server = http.createServer(function (request, response) {
 
 // listen on localhost:8000
 server.listen(8000);
-console.log("Server listening on port 8000 :  http://127.0.0.1:8000/");
+console.log("Server listening on port 8000 and it's not bad :  http://127.0.0.1:8000/");
